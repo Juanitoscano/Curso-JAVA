2fc81cb228f30bb583e044de4ce19fa3bd953d3f
Juan Toscano <140565794+Juanitoscano@users.noreply.github.com>
2024-05-06 20:51:22 -0600

    Add files via upload

---
+++ Java/DeclaracionesOperadores.java
@@ -0,0 +1,32 @@
+public class DeclaracionesOperadores {
+    public static void main(String[] args) {
+        
+        /*int a=5;
+    
+        int b;
+
+        b=10;
+
+        int c=b+a;
+
+        c-=6;
+
+        final double apulgadas=2.54;
+
+        double cm=6;
+
+        double resultado=cm/apulgadas;
+
+        System.out.println("En " + cm + " cm hay " + resultado + " pulgadas");
+        */
+        int operador1,operador2, resultado;
+
+        operador1=9;
+
+        operador2=7;
+
+        resultado=operador1+operador2;
+
+        System.out.println(resultado);
+    }
+}
