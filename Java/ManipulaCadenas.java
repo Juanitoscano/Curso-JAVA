0e0d23d9478f0e7c9793ab4cb4ed0937e8c387a1
Juan Toscano <140565794+Juanitoscano@users.noreply.github.com>
2024-05-07 19:02:21 -0600

    Add files via upload

---
+++ Java/ManipulaCadenas.java
@@ -0,0 +1,22 @@
+public class ManipulaCadenas {
+
+    public static void main(String[] args) {
+        
+        String nombre="Juan";
+
+        System.out.println("Mi nombre es " + nombre);
+
+        System.out.println("Mi nombre tiene " + nombre.length() + " letras");
+        
+        System.out.println("La primera letra de " + nombre + " es la " + nombre.charAt(0));
+
+        int ultima_letra;
+
+        ultima_letra=nombre.length();
+
+        System.out.println("Y la ultima letra es la " + nombre.charAt(ultima_letra-1));
+
+
+    }
+    
+}
