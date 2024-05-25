9f0aa402a0a46a58850b4be3b7f3b62a2012f5f6
Juan Toscano <140565794+Juanitoscano@users.noreply.github.com>
2024-05-24 20:33:44 -0600

    Add files via upload

---
+++ Java/UsoArray.java
@@ -0,0 +1,22 @@
+public class UsoArray {
+    public static void main(String[] args) {
+        
+       /* int[] matriz = new int[5];
+
+        matriz[0]=5;
+        matriz[1]=7;
+        matriz[2]=10;
+        matriz[3]=51;
+        matriz[4]=76;*/
+
+        int [] matriz={5,7,10,51,76,88,36,34,53,16,28};
+
+        for(int i=0;i<matriz.length;i++){
+
+            System.out.println("Valor del índice "+ i + " = " + matriz[i]);
+
+        }
+
+    }
+    
+}
